577013a1154ba93cb1b755770d04422deb73b767
wujinxiang <wujinxiang@tcsl.com.cn>
2020-10-23 17:54:31 +0800

    增加分类页面和对应的接口处理

--- lib/page/category_page.dart
+++ lib/page/category_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_shop/model/category.dart';
+import 'package:flutter_shop/service/service_method.dart';
 
 class CategoryPage extends StatefulWidget {
   CategoryPage({Key key}) : super(key: key);
@@ -9,11 +12,83 @@ class CategoryPage extends StatefulWidget {
 
 class _CategoryPageState extends State<CategoryPage> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("商城页面"),
+      appBar: AppBar(
+        title: Text("商品分类"),
+      ),
+      body: Container(
+        child: Row(
+          children: [
+            LeftCategoryNav(),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class LeftCategoryNav extends StatefulWidget {
+  LeftCategoryNav({Key key}) : super(key: key);
+
+  @override
+  _LeftCategoryNavState createState() => _LeftCategoryNavState();
+}
+
+class _LeftCategoryNavState extends State<LeftCategoryNav> {
+  List list;
+
+  @override
+  void initState() {
+    _getCategory();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ScreenUtil().setWidth(200),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(right: BorderSide(width: 1, color: Colors.black12))),
+      child: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _leftNav(list[index]);
+        },
+      ),
+    );
+  }
+
+  Widget _leftNav(CategoryBigModel item) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        child: Text(item.mallCategoryName,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: ScreenUtil().setSp(26))),
+        alignment: Alignment.center,
+        height: ScreenUtil().setHeight(80),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border:
+                Border(bottom: BorderSide(width: 1, color: Colors.black12))),
+      ),
+    );
+  }
+
+  void _getCategory() {
+    request('category').then((value) {
+      CategoryBigListModel categoryBigListModel =
+          CategoryBigListModel.fromJson(value['data']);
+      setState(() {
+        list = categoryBigListModel.data;
+      });
+    });
   }
 }
