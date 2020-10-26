30cd075c0643d1e5e822f88f0501927b1d35b68f
wujinxiang <wujinxiang@tcsl.com.cn>
2020-10-26 17:25:01 +0800

    详情页面切换

--- lib/page/detail/detail_web.dart
+++ lib/page/detail/detail_web.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_shop/provide/details_info.dart';
 import 'package:provide/provide.dart';
 
@@ -11,9 +12,19 @@ class DetailWeb extends StatelessWidget {
     return Provide<DetailsInfoProvide>(builder: (context, child, value) {
       var info = value.goodsInfo;
       if (info != null) {
-        return Container(
-          child: Html(data: info.data.goodInfo.goodsDetail),
-        );
+        var left = value.isLeft;
+        if (left) {
+          return Container(
+            child: Html(data: info.data.goodInfo.goodsDetail),
+          );
+        } else {
+          return Container(
+            width: ScreenUtil().setWidth(720),
+            padding: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            child: Text("暂时没有数据"),
+          );
+        }
       } else {
         return Container();
       }
